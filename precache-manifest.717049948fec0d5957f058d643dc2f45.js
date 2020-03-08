f95a0f2c9ee40cd56d862f92da9f1707cd190d49
Jon <jong649@gmail.com>
2020-03-07 22:30:15 -0800

    Updates

---
+++ precache-manifest.717049948fec0d5957f058d643dc2f45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29d92777126bd8ba9264a067884a8ad",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "b81d70156b0564748551",
+    "url": "/todo-list/static/css/main.57a697e9.chunk.css"
+  },
+  {
+    "revision": "556fd5b044c8a55a44b6",
+    "url": "/todo-list/static/js/2.1066fcfa.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/todo-list/static/js/2.1066fcfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81d70156b0564748551",
+    "url": "/todo-list/static/js/main.1aa1d515.chunk.js"
+  },
+  {
+    "revision": "492b6893fe135a4273bc",
+    "url": "/todo-list/static/js/runtime-main.7e9ae0c4.js"
+  }
+]);
